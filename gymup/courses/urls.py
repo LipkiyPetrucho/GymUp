3c90cfb35f8a94b7bef2918113416fecfe36a983
Petr <bang1987@mail.ru>
2024-06-20 19:49:30 +0400

    start project GymUp. Models be added in app courses

---
+++ gymup/courses/urls.py
@@ -0,0 +1,9 @@
+from . import views
+from django.urls import path
+
+urlpatterns = [
+    path('mine/', views.ManageCourseListView.as_view(), name='manage_course_list'),
+    path('create/', views.CourseCreateView.as_view(), name='course_create'),
+    path('<pk>/edit/', views.CourseUpdateView.as_view(), name='course_edit'),
+    path('<pk>/delete/', views.CourseDeleteView.as_view(), name='course_delete'),
+]
--- gymup/gymup/settings.py
+++ gymup/gymup/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -30,6 +31,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # packages install
+    'django_bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -108,7 +110,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'courses', 'static')
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
